717527dbf0817b2696d95c3f637749d9fae7ed13
Rohitbhakuni88 <rohitbhakuni797@gmail.com>
2024-11-26 03:14:03 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+document.addEventListener("DOMContentLoaded", function () {
+    var typed = new Typed(".typing", {
+      strings: [
+        '<b><span style="color:orange;">INTER</span><span style="color:white">VIEW</span> <span style="color: lightgreen;">READYIFY</span></b>'
+
+      ],
+      typeSpeed: 100,
+      backSpeed: 60,
+      loop: true,
+    });
+  });
+  
